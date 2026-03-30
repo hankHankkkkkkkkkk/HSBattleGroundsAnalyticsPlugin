ca6c7e6c34f34d38d45a28c79289da8876f4f4b9
hankHankkkkkkkkkk <jinghan.wangg@outlook.com>
2026-03-30 20:14:43 +0200

    feat: add battleground results window with match list entry

---
+++ HDT_plugins/Models/BgMatchRow.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Media;
+
+namespace HDTplugins.Models
+{
+    public class BgMatchRow
+    {
+        public string MatchId { get; set; }
+        public int Placement { get; set; }
+        public int RatingAfter { get; set; }
+        public int RatingDelta { get; set; }
+        public string HeroName { get; set; }
+
+        public string AnomalyDisplay { get; set; }
+        public string FinalBoardDisplay { get; set; }
+
+        public string RatingDeltaText
+        {
+            get
+            {
+                if (RatingDelta > 0)
+                    return "+" + RatingDelta;
+                return RatingDelta.ToString();
+            }
+        }
+
+        public Brush RatingDeltaBrush
+        {
+            get
+            {
+                if (RatingDelta > 0)
+                    return Brushes.ForestGreen;
+                if (RatingDelta < 0)
+                    return Brushes.IndianRed;
+                return Brushes.Gray;
+            }
+        }
+    }
+}
