5fef71b0d614536a201a65fac7de6ee96ddcf048
hankHankkkkkkkkkk <jinghan.wangg@outlook.com>
2026-03-30 20:42:56 +0200

    fix: resolve build errors and bump plugin version to 0.5.1

--- HDT_plugins/Models/BgSnapshot.cs
+++ HDT_plugins/Models/BgSnapshot.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HDT_plugins.Models
+namespace HDTplugins.Models
 {
     internal class BgSnapshot
     {
+        public string MatchId { get; set; }
+        public string Timestamp { get; set; }
+        public string GameVersion { get; set; }
+
+        public string HeroCardId { get; set; }
+        public string HeroName { get; set; }
+        public string HeroSkinCardId { get; set; }
+        public string HeroPowerCardId { get; set; }
+
+        public int RatingBefore { get; set; }
+        public int RatingAfter { get; set; }
+        public int RatingDelta { get; set; }
+        public int Placement { get; set; }
+
+        public string[] AvailableRaces { get; set; } = Array.Empty<string>();
+        public string AnomalyCardId { get; set; }
+        public string AnomalyName { get; set; }
+        public string[] FinalBoardCardIds { get; set; } = Array.Empty<string>();
     }
 }
